b8a6832e8db4db023849096ea2d4e275c5d93289
get-Friday <gabi.get@hotmail.com>
2022-05-13 10:46:32 -0300

    ngForm validations

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -7,12 +7,12 @@ import { Component, OnInit } from "@angular/core";
 			<label for="username">Username</label>
 			<input type="text" name="username" id="username" [(ngModel)]="username" />
 			<label for="cpf">CPF</label>
-			<input type="number" name="cpf" id="cpf" [(ngModel)]="cpf">
+			<input type="number" name="cpf" id="cpf" [(ngModel)]="cpf" required>
 			<label for="birthday">Dia de aniversário</label>
-			<input type="date" name="birthday" id="birthday" [(ngModel)]="birthday">
+			<input type="date" name="birthday" id="birthday" [(ngModel)]="birthday" max="31" min="1">
 			<label for="password">Senha</label>
-			<input type="password" name="password" id="password" [(ngModel)]="password">
-			<button type="submit">Mostrar</button>
+			<input type="password" name="password" id="password" [(ngModel)]="password" required>
+			<button type="submit" [disabled]="!form.form.valid">Mostrar</button>
 		</form>
 
 		<br />
@@ -26,10 +26,10 @@ import { Component, OnInit } from "@angular/core";
 	styles: [],
 })
 export class FormComponent implements OnInit {
-	username: string = "";
-	cpf: number = 0;
-	birthday: number = 0;
-	password: number = 0;
+	username: string;
+	cpf: Number;
+	birthday: number;
+	password: number;
 	toShow: boolean = false;
 
 	constructor() {}
